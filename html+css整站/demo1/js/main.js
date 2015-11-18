7231f4cf1e279e21c140f74b572841f6d5416959
Andy Zheng <azheng@objectivasoftware.com>
2015-11-18 17:38:38 +0800

    add a new project.

--- html+css整站/demo1/js/main.js
+++ html+css整站/demo1/js/main.js
@@ -53,6 +53,19 @@ x.ui = {
         obj.style.opacity = value/100;
       }
     }, 30);
+  },
+  moveLeft: function(obj, fromVal, toVal) {
+    clearInterval(obj.timer);
+    obj.timer = setInterval(function() {
+      var iSpeed = (toVal - fromVal) / 10;
+      iSpeed = iSpeed > 0 ? Math.ceil(iSpeed) : Math.floor(iSpeed);
+      if (toVal == fromVal) {
+        clearInterval(obj.timer);
+      } else {
+        fromVal += iSpeed;
+        obj.style.left = fromVal + 'px';
+      }
+    }, 30);
   }
 }
 
@@ -118,17 +131,65 @@ x.app = {
     var oDDs = oSel.getElementsByTagName('dd');
     var oAs = oSel.getElementsByTagName('a');
     var oUl = oSel.getElementsByTagName('ul');
+    var oH2 = oSel.getElementsByTagName('h2');
     for (var i = 0; i < oAs.length; i++) {
       oAs[i].index = i;
       oAs[i].onclick = function(ev) {
         var e = ev || event;
+        // 先隐藏其他的下拉框
+        for (var i = 0; i < oUl.length; i++) {
+          oUl[i].style.display = 'none';
+        }
+        var This = this;
         oUl[this.index].style.display = 'block';
         event.cancelBubble = true;
+        document.onclick = function() {
+          oUl[This.index].style.display = 'none';
+        }
       }
-      document.
-      document.onclick = function(i) {
-        oUl[].style.display = 'none';
+    }
+    for (var i = 0; i < oUl.length; i++) {
+      oUl[i].index = i;
+      bindEventForDropDown(oUl[i]);
+    }
+
+    function bindEventForDropDown(ul) {
+      var oLis = ul.getElementsByTagName('li');
+      for (var i = 0; i < oLis.length; i++) {
+        oLis[i].onclick = function(ev) {
+          var e = ev || event;
+          e.cancelBubble = true;
+          oH2[ul.index].innerHTML = this.innerHTML;
+          ul.style.display = 'none';
+        }
       }
+    }
+  },
+  // 初始化轮播图的事件
+  initCarousel: function() {
+    var oCar = document.getElementById('scroll-list');
+    var oUl = oCar.getElementsByTagName('ul')[0];
+    var oLis = oUl.getElementsByTagName('li');
+    var oPrev = x.tools.getByClass(oCar, 'prev')[0];
+    var oNext = x.tools.getByClass(oCar, 'next')[0];
+    oUl.innerHTML += oUl.innerHTML; // 拷贝了一份
+    oUl.style.width = oLis.length * oLis[0].offsetWidth + 'px';
+    var iNow = 0;
+    oPrev.onclick = function() {
+      if (iNow == 0) {
+        iNow = oLis.length / 2;
+        oUl.style.left = -oUl.offsetWidth / 2 + 'px';
+      }
+      x.ui.moveLeft(oUl, -iNow*oLis[0].offsetWidth, -(iNow - 1) * oLis[0].offsetWidth);
+      iNow--;
+    }
+    oNext.onclick = function() {
+      if (iNow == oLis.length / 2) {
+        iNow = 0;
+        oUl.style.left = 0;
+      }
+      x.ui.moveLeft(oUl, -iNow * oLis[0].offsetWidth, -(iNow + 1) * oLis[0].offsetWidth);
+      iNow++;
     }
   }
 
@@ -137,4 +198,5 @@ x.app = {
 window.onload = function() {
   x.app.initBanner();
   x.app.initSell();
+  x.app.initCarousel();
 }
