623f3f29c77ce321d3aad04bb82b2fb97fca06ca
Andy Zheng <azheng@objectivasoftware.com>
2015-11-27 18:02:51 +0800

    completed the eCommerce site html and css.

--- 电商网站demo/js/app.js
+++ 电商网站demo/js/app.js
@@ -1,3 +1,34 @@
 $(function() {
-
+	// 搜索框
+	(function(){
+		var oLis = $('#search li');
+		var oTxt = $('#search form .text');
+		var txt = [
+			'例如：荷棠鱼坊烧鱼 或 樱花日本料理',
+			'例如：昌平区育新站龙旗广场2号楼609室',
+			'例如：万达影院双人情侣券',
+			'例如：东莞出事了，大老虎是谁？',
+			'例如：北京初春降雪，天气变幻莫测'
+		];
+		var iNow = 0;
+		oTxt.val(txt[iNow]);
+		oLis.each(function(index) {
+			$(this).click(function(event) {
+				oLis.removeClass('active');
+				$(this).addClass('active');
+				iNow = index;
+				oTxt.val(txt[iNow]);
+			});
+		});
+		oTxt.focus(function(event) {
+			if (this.value == txt[iNow]) {
+				this.value = '';
+			};
+		});
+		oTxt.blur(function(event) {
+			if (!this.value) {
+				this.value = txt[iNow];
+			};
+		});
+	})();
 });
